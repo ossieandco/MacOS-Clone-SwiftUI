acdbfcfa3c76070d7716c177696b62c171be4d02
Pallav Agarwal <agpallav@gmail.com>
2025-11-20 23:09:31 -0500

    Initial Commit

---
+++ MacOSClone/MacOSCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSCloneApp.swift
+//  MacOSClone
+//
+//  Created by Pallav Agarwal on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MacOSCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
